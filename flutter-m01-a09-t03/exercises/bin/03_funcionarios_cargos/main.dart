16ddedf5208df50ab1fe73a5cc7f624f752585de
Mayara Gualberto <mayaragualberto@yahoo.com.br>
2022-09-27 21:40:37 -0300

    aula-27-09

---
+++ flutter-m01-a09-t03/exercises/bin/03_funcionarios_cargos/main.dart
@@ -0,0 +1,10 @@
+void main() {
+  // 1. Declaração de variáveis
+  
+  // 2. Atribuição de valores (ENTRADA)
+  
+  // 3. Validações e Cálculos (PROCESSAMENTO)
+  
+  // 4. Mostra o resultado (SAÍDA)
+  
+}
